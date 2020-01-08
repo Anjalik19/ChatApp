314e79687410105424929130382a1da95b1e6f7c
Anjalik19 <anjali.march19@gmail.com>
2020-01-08 13:31:56 +0530

    [Anjali] Refactor login page

--- Client/chatapp/src/Component/login.jsx
+++ Client/chatapp/src/Component/login.jsx
@@ -1,58 +1,60 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom';
 import { Card, TextField, Button } from '@material-ui/core'
+import Link from '@material-ui/core/Link';
+import Grid from '@material-ui/core/Grid';
 export class login extends Component {
-    constructor(){
+    constructor() {
         super();
-        this.state={
-        email:'',
-        password:''
+        this.state = {
+            email: '',
+            password: ''
         }
 
-       }
-       handleRegister=()=>{
+    }
+    handleRegister = () => {
         this.props.history.push('/register')
     }
     render() {
         return (
             <div className="login_container">
                 <center>
-             <Card className="loginCard"  style={{justifyContent:'center' , width: '30em',height: '60vh'}}>  
-              <center>
-               <div>
-                   <h2>Member Login</h2>
-               </div>
-               
-                 <TextField
-                     id="outlined-basic"
-                     label="email"
-                     variant="standard"
-                     type="email">
-                </TextField>
-                 <br/>
-                 <TextField
-                    id="outlined-basic"
-                    label="Password"
-                    variant="standard"
-                    type="password">
-                 </TextField>
-                 <br/>
-                 </center>
-                    <div>
-                    <center>
-                     <Button className="login_Btn" variant="contained" color="primary">
-                        login
-                    </Button>
-                    <Button className="login_Btn" variant="contained" color="primary">
-                        Sign Up
-                    </Button>
-                    <div className="forgot_password" variant="contained" color="primary" placeholder="bottem-left">
-                    <h5>Forgot Password</h5>
-                 </div>
-                    </center>
-                    </div>
-             </Card>
-             </center>
+                    <Card className="loginCard" style={{ justifyContent: 'center', width: '30em', height: '60vh',marginTop: '100px',backgroundColor: 'grey' }}>
+                     
+                            <div style={{marginTop: '60px'}}> 
+                                <h2>Member Login</h2>
+                            </div>
+              
+                            <TextField
+                                id="outlined-basic"
+                                label="email"
+                                variant="standard"
+                                type="email">
+                            </TextField>
+                            <br />
+                            <TextField
+                                id="outlined-basic"
+                                label="Password"
+                                variant="standard"
+                                type="password">
+                            </TextField>
+                            <br />
+                    
+                        <div style={{ marginTop: "10px" , marginBottom: "20px"}}>
+                            <Button className="Btn" variant="contained" color="primary" style={{ width: "16em" }}>
+                                login
+                            </Button>
+                        </div>
+                        <Grid style={{ marginTop: "10px" , marginBottom: "20px"}}> 
+                          Don't have an account  <Link href="/register" >Sign Up</Link>
+                        </Grid>
+                        <Grid>
+                                <Link href="" variant="body2">
+                                    Forgot password?
+                                 </Link>
+                        </Grid>
+                    </Card>
+                </center>
             </div>
         )
     }
--- Client/chatapp/src/Component/register.jsx
+++ Client/chatapp/src/Component/register.jsx
@@ -6,100 +6,57 @@ class register extends Component {
     constructor() {
         super();
         this.state = {
-            first_name: '',
-            last_name:'',
+            name: '',
             email: '',
             password: '',
             password_confirm: '',
-            errors: {}
         }
-        this.handleInputChange = this.handleInputChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
-
-    handleInputChange(event) {
-        this.setState({
-            [event.target.name]: event.target.value
-        })
-    }
-
-    handleSubmit(event) {
-        event.preventDefault();
-        const user = {
-            name: this.state.name,
-            email: this.state.email,
-            password: this.state.password,
-            password_confirm: this.state.password_confirm
-        }
-        console.log(user);
-    }
-
     render() {
         return (
 
             <div className="container" style={{ marginTop: '50px', width: '500px' ,height: '700px'}}>
-                 <center>
+                <center>
                     <Card>
+                        
                         <h2 style={{ marginBottom: '40px' }}>Registration</h2>
-                        <form onSubmit={this.handleSubmit}>
                             <div className="form-group">
                             <TextField id="standard-basic" 
                             type="text"
-                            placeholder="First Name"
-                            first_name="first_name"
-                            onChange={this.handleInputChange}
-                            value={this.state.first_name}
+                            placeholder="Name"
+                            name="name"
                             />
                             </div>
-
-                            <div className="form-group">
-                            <TextField id="standard-basic" 
-                            type="text"
-                            placeholder= "Last Name"
-                            last_name="last_name"
-                            onChange={this.handleInputChange}
-                            value={this.state.last_name}
-                            />
-                            </div>
-
                             <div className="form-group">
                             <TextField id="standard-basic" 
                                     type="email"
                                     placeholder="Email"
-                                    name="email"
-                                    onChange={this.handleInputChange}
-                                    value={this.state.email}
-                                />
+                                   />
                             </div>
 
                             <div className="form-group">
                             <TextField id="standard-basic" 
                                     type="password"
                                     placeholder="Password"
-                                    className="form-control"
-                                    name="password"
-                                    onChange={this.handleInputChange}
-                                    value={this.state.password}
-                                />
+                                    />
                             </div>
-
                             <div className="form-group">
                             <TextField id="standard-basic" 
                                     type="password"
                                     placeholder="Confirm Password"
                                     className="form-control"
-                                    name="password_confirm"
-                                    onChange={this.handleInputChange}
-                                    value={this.state.password_confirm}
+                                 
                                 />
                             </div>
-
                             <div className="form-group">
                                 <button type="submit" >
                                     Register User
                     </button>
+                    <button type="submit" >
+                                    Cancel
+                    </button>
                             </div>
-                        </form>
+                        
                     </Card>
                     </center>
             </div>
