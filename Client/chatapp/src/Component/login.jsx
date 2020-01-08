c4bb084b14d32305b70702fabb14101d018a2040
Anjalik19 <anjali.march19@gmail.com>
2020-01-08 15:16:09 +0530

    [Anjali] Refactor login page logic

--- Client/chatapp/src/Component/login.jsx
+++ Client/chatapp/src/Component/login.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { withRouter } from 'react-router-dom';
-import { Card, TextField, Button } from '@material-ui/core'
+import { Card, TextField, Button, Checkbox } from '@material-ui/core'
 import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
 export class login extends Component {
@@ -19,38 +19,45 @@ export class login extends Component {
         return (
             <div className="login_container">
                 <center>
-                    <Card className="loginCard" style={{ justifyContent: 'center', width: '30em', height: '60vh',marginTop: '100px',backgroundColor: 'grey' }}>
-                     
-                            <div style={{marginTop: '60px'}}> 
-                                <h2>Member Login</h2>
-                            </div>
-              
-                            <TextField
-                                id="outlined-basic"
-                                label="email"
-                                variant="standard"
-                                type="email">
-                            </TextField>
-                            <br />
-                            <TextField
-                                id="outlined-basic"
-                                label="Password"
-                                variant="standard"
-                                type="password">
-                            </TextField>
-                            <br />
-                    
-                        <div style={{ marginTop: "10px" , marginBottom: "20px"}}>
-                            <Button className="Btn" variant="contained" color="primary" style={{ width: "16em" }}>
+                    <Card className="loginCard" style={{ justifyContent: 'center', width: '30em', height: '70vh', marginTop: '100px', backgroundColor: 'grey' }}>
+                        <div style={{ marginTop: '60px' }}>
+                            <h2>Member Login</h2>
+                        </div>
+                        <TextField
+                            id="outlined-basic"
+                            label="email"
+                            variant="standard"
+                            type="email">
+                        </TextField>
+
+                        <br />
+
+                        <TextField
+                            id="outlined-basic"
+                            label="Password"
+                            variant="standard"
+                            type="password">
+                        </TextField>
+
+                        <br />
+
+                        <div style={{ marginTop: '10px', marginRight: "80px" }}>
+                            <Checkbox value="remember" color="primary" />Remember me
+                        </div>
+
+                        <div style={{ marginTop: "10px", marginBottom: "20px" }}>
+                            <Button className="Btn" onClick={this.handleRegister} variant="contained" color="primary" style={{ width: "16em" }}>
                                 login
                             </Button>
                         </div>
-                        <Grid style={{ marginTop: "10px" , marginBottom: "20px"}}> 
-                          Don't have an account  <Link href="/register" >Sign Up</Link>
+
+                        <Grid style={{ marginTop: "10px", marginBottom: "20px" }}>
+                            Don't have an account  <Link href="/register" >Sign Up</Link>
                         </Grid>
+
                         <Grid>
-                                <Link href="" variant="body2">
-                                    Forgot password?
+                            <Link href="" variant="body2">
+                                Forgot password?
                                  </Link>
                         </Grid>
                     </Card>
--- Client/chatapp/src/Component/register.jsx
+++ Client/chatapp/src/Component/register.jsx
@@ -12,56 +12,65 @@ class register extends Component {
             password_confirm: '',
         }
     }
+    handleRegister = () => {
+        this.props.history.push('/login')
+    }
     render() {
         return (
+            <div className="login_container">
+            <center>
+            <Card style={{justifyContent: 'center',width: '30em', height:'70vh',marginTop: '60px'}}>
+                    
+                    <div style={{marginTop: '50px'}}>
 
-            <div className="container" style={{ marginTop: '50px', width: '500px' ,height: '700px'}}>
-                <center>
-                    <Card>
-                        
-                        <h2 style={{ marginBottom: '40px' }}>Registration</h2>
-                            <div className="form-group">
-                            <TextField id="standard-basic" 
-                            type="text"
-                            placeholder="Name"
-                            name="name"
-                            />
-                            </div>
-                            <div className="form-group">
-                            <TextField id="standard-basic" 
-                                    type="email"
-                                    placeholder="Email"
-                                   />
-                            </div>
+                    <h2>Registration</h2>
 
-                            <div className="form-group">
-                            <TextField id="standard-basic" 
-                                    type="password"
-                                    placeholder="Password"
-                                    />
-                            </div>
-                            <div className="form-group">
-                            <TextField id="standard-basic" 
-                                    type="password"
-                                    placeholder="Confirm Password"
-                                    className="form-control"
-                                 
+                    </div>
+                        <div className="form-group" style={{marginBottom: '10px'}}>
+                        <TextField id="standard-basic" 
+                        type="text"
+                        placeholder="Name"
+                        name="name"
+                        />
+                        </div>
+                        <div className="form-group" style={{marginBottom: '10px'}}>
+                        <TextField id="standard-basic" 
+                                type="email"
+                                placeholder="Email"
+                               />
+                        </div>
+
+                        <div className="form-group" style={{marginBottom: '10px'}}>
+                        <TextField id="standard-basic" 
+                                type="password"
+                                placeholder="Password"
                                 />
-                            </div>
-                            <div className="form-group">
-                                <button type="submit" >
-                                    Register User
-                    </button>
-                    <button type="submit" >
-                                    Cancel
-                    </button>
-                            </div>
-                        
-                    </Card>
-                    </center>
+                        </div>
+                        <div className="form-group" style={{marginBottom: '60px'}}>
+                        <TextField id="standard-basic" 
+                                type="password"
+                                placeholder="Confirm Password"
+                                className="form-control"
+                             
+                            />
+                        </div>
+                        <div className="form-group">
+                            <button type="submit" style={{marginRight:'50px'}}>
+                                Register User
+                </button>
+                <button type="submit" onClick={this.handleRegister}>
+                                Cancel
+                </button>
+                        </div>
+
+                </Card>
+            </center>
             </div>
         )
     }
 }
-
 export default withRouter(register);
+
+
+
+
